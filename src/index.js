57c1aaaaca9ea733efac9faebd2efeb2127bbe98
Imon <contact@imonweb.co.uk>
2023-05-17 22:37:49 +0100

    components

--- src/index.js
+++ src/index.js
@@ -13,4 +13,10 @@ const Greeting = () => {
   return  <h2>My First Component</h2>
 }
 
-export default Greeting;
+// export default Greeting;
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<Greeting></Greeting>)
+
+ 

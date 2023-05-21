a9a8c25065b97fe6109ddc44caf04614da2217d8
Imon <contact@imonweb.co.uk>
2023-05-21 09:22:35 +0100

    props

--- src/index.js
+++ src/index.js
@@ -2,30 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import book1 from "./images/book-1.jpg";
+import book2 from "./images/book-2.jpg";
  
+const firstBook = {
+  author: 'Jordan Moore',
+  title: 'Interesting Facts For Curious Minds',
+  img: './images/book-1.jpg'
+}
+
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: './images/book-2.jpg'
+}
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-     
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.book1} >
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <button>click me</button>
+      </Book>
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.book2} />
     </section>
   )
 }
 
-const Image = () => (
-  <img src={book1} alt='Interesting Facts For Curious Minds' />
-);
-
-const Book = () => {
+const Book = (props) => {
+  // const {img, title, author} = props
+  const {img, title, author, children} = props;
+  console.log(props);
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className='book'>
+      <img src={book1} alt={title} />
+       <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   )
 }
@@ -33,10 +45,8 @@ const Book = () => {
 
 
 // const Image = () =>  <img src='https://m.media-amazon.com/images/I/51x8pmqjY0L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg' />  
-const Title = () => {
-  return <h2>Interesting Facts For Curious Minds</h2>
-}
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}}>Jordan Moore</h4>
+ 
+ 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 

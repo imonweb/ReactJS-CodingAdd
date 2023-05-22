2f9656f0f93940aaff86d4fdf0d67c57dedd3e35
Imon <contact@imonweb.co.uk>
2023-05-22 17:38:10 +0100

    numbers

--- src/index.js
+++ src/index.js
@@ -1,84 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import book1 from "./images/book-1.jpg";
-import book2 from "./images/book-2.jpg";
-
-const books = [
-  {
-    author: 'Jordan Moore',
-    title: 'Interesting Facts For Curious Minds',
-    img: './images/book-1.jpg',
-    id: 1
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: './images/book-2.jpg',
-    id: 2
-  }
-]
+import { books } from './books'; 
+import Book from './Book';
  
 const BookList = () => {
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id)
+    console.log(book);
+  }
   return (
     <section className='booklist'>
-      <EventExamples />
-      {books.map((book)=> {
+      {/* <EventExamples /> */}
+      {books.map((book, index)=> {
         return (
-          <Book {...book} key={book.id}/>
+          <Book {...book} key={book.id} getBook={getBook} number={index} />
         )
       })}
+    
     </section>
   )
 }
 
-const EventExamples = () => {
-  const handleFormInput = (e) => {
-    console.log(e.target)
-    console.log(e.target.name)
-    console.log(e.target.name.value)
-    console.log('handle form input');
-  }
-  const handleButtonClick = () => {
-    alert('handle button click')
-  }
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    console.log('form submitted')
-  }
+// const EventExamples = () => {
+//   const handleFormInput = (e) => {
+//     console.log(e.target)
+//     console.log(e.target.name)
+//     console.log(e.target.name.value)
+//     console.log('handle form input');
+//   }
+//   const handleButtonClick = () => {
+//     alert('handle button click')
+//   }
+//   const handleFormSubmission = (e) => {
+//     e.preventDefault();
+//     console.log('form submitted')
+//   }
 
-  return <section>
-    <form onSubmit={handleFormSubmission}>
-      <h2>Typical Form</h2>
-      <input type='text' name='product' onChange={(e) => console.log(e.target.value)} style={{margin: '1rem 0'}} /> 
-    <button type="submit">submit</button>
-    <button onClick={() => {
-      console.log('click me')
-    }}>click me</button>
-    </form>
-    <div>
+//   return <section>
+//     <form onSubmit={handleFormSubmission}>
+//       <h2>Typical Form</h2>
+//       <input type='text' name='product' onChange={(e) => console.log(e.target.value)} style={{margin: '1rem 0'}} /> 
+//     <button type="submit">submit</button>
+//     <button onClick={() => {
+//       console.log('click me')
+//     }}>click me</button>
+//     </form>
+//     <div>
 
-    </div>
-  </section>
-}
-
-const Book = (props) => {
-  // const {img, title, author} = props
-  const {img, title, author} = props;
-  console.log(props);
-  const displayTitle = () => {
-    console.log(title);
-  }
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={displayTitle}>display title</button>
-      <h4>{author}</h4>
- 
-    </article>
-  )
-}
+//     </div>
+//   </section>
+// }
 
 
 
